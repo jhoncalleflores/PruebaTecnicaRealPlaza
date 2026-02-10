0918e6c97097ae81bb2d030cf1d750341cd1f1bb
jhoncalleflores <124798859+jhoncalleflores@users.noreply.github.com>
2026-02-10 09:14:50 -0500

    Update Program.cs

--- Caso 3/RealPlaza.API/Program.cs
+++ Caso 3/RealPlaza.API/Program.cs
@@ -1,6 +1,20 @@
+using Dapper;
 using RealPlaza.API.Extensions;
+using RealPlaza.Infrastructure.Data;
+
+SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -8,6 +22,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationServices();
 
 var app = builder.Build();
+
+app.UseCors("AllowReact");
 
 app.UseSwagger();
 app.UseSwaggerUI();
